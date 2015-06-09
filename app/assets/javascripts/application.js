642dfb10c2dc3f9fef5dd068b34c959aa80b77c2
Charlie Young <charles.alden.young@gmail.com>
2015-06-08 20:00:03 -0700

    Added Outbound Tracking Script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,5 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+<script type="text/javascript"> window.outbound=window.outbound||[];window.outbound.methods=["identify","track","registerApnsToken","registerGcmToken","disableApnsToken","disableGcmToken"];window.outbound.factory=function(t){return function(){var o=Array.prototype.slice.call(arguments);o.unshift(t);window.outbound.push(o);return window.outbound}};for(var i=0;i<window.outbound.methods.length;i++){var key=window.outbound.methods[i];window.outbound[key]=window.outbound.factory(key)}if(!document.getElementById("outbound-js")){var script=document.createElement("script");script.type="text/javascript";script.id="outbound-js";script.async=true;script.src="//cdn.outbound.io/pub-83458db70404d4221c66878c35ac80f8.js";var first=document.getElementsByTagName("script")[0];first.parentNode.insertBefore(script,first)} </script>
